4a5e9d4040c01388de1b1c6f5b42a78d4d0bc185
JAF-Patrick <jpaguiap@jafdigital.co>
2025-07-02 12:32:52 +0800

    Bulk Attendance 1 07/02/2025 12:32PM

--- database/migrations/2025_07_02_112806_create_bulk_attendances_table.php
+++ database/migrations/2025_07_02_112806_create_bulk_attendances_table.php
@@ -12,8 +12,28 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('bulk_attendances', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->date('date_from');
+            $table->date('date_to');
+            $table->integer('regular_working_days')->nullable();
+            $table->integer('regular_working_hours')->nullable();
+            $table->integer('regular_overtime_hours')->nullable();
+            $table->integer('regular_nd_hours')->nullable();
+            $table->integer('regular_nd_overtime_hours')->nullable();
+            $table->integer('rest_day_work')->nullable();
+            $table->integer('rest_day_ot')->nullable();
+            $table->integer('rest_day_nd')->nullable();
+            $table->integer('regular_holiday_hours')->nullable();
+            $table->integer('special_holiday_hours')->nullable();
+            $table->integer('regular_holiday_ot')->nullable();
+            $table->integer('special_holiday_ot')->nullable();
+            $table->integer('regular_holiday_nd')->nullable();
+            $table->integer('special_holiday_nd')->nullable();
             $table->timestamps();
+
+            // Foreign key constraint
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
