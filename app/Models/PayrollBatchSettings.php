79020328229df5eeed7592aa6fb77a72bc5b5684
John Patrick Paguia <johnpatrickpaguia@Johns-MacBook-Air.local>
2025-07-30 12:12:47 +0800

    PayrollBatch on Payroll Process (SP and Main) 07/30/2025

--- app/Models/PayrollBatchSettings.php
+++ app/Models/PayrollBatchSettings.php
@@ -7,15 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class PayrollBatchSettings extends Model
 {
     protected $table = 'payroll_batch_settings';
+
     protected $fillable = [
-        'name',  
+        'name',
         'tenant_id'
     ];
+
     public $timestamps = true;
 
     public function batchUsers()
     {
         return $this->hasMany(PayrollBatchUsers::class, 'pbsettings_id');
     }
-
 }
