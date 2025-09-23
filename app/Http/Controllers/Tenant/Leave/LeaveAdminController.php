b8ca7e35c6f53af3aed2bdec4aac96580c951444
Rodel Santos <rodelsantos1999@gmail.com>
2025-09-23 11:35:45 +0800

    Fixed Leave Admin Approval (Staging affiliate) 9/23/2025 11:35AM

--- app/Http/Controllers/Tenant/Leave/LeaveAdminController.php
+++ app/Http/Controllers/Tenant/Leave/LeaveAdminController.php
@@ -110,7 +110,11 @@ class LeaveAdminController extends Controller
                 $lr->remaining_balance = 0; // Default to 0 if no entitlement is found
             }
         }
-
+        $fullname = trim($authUser->personalInformation->first_name . ' ' . $authUser->personalInformation->last_name);
+ 
+        $leaveRequests = $leaveRequests->filter(function ($lr) use ($fullname) {
+            return in_array($fullname, $lr->next_approvers ?? []);
+        })->values();
 
         $html = view('tenant.leave.adminleave_filter', compact('leaveRequests', 'permission'))->render();
 
@@ -209,6 +213,11 @@ class LeaveAdminController extends Controller
                 $lr->remaining_balance = 0; // Default to 0 if no entitlement is found
             }
         }
+        $fullname = trim($authUser->personalInformation->first_name . ' ' . $authUser->personalInformation->last_name);
+ 
+        $leaveRequests = $leaveRequests->filter(function ($lr) use ($fullname) {
+            return in_array($fullname, $lr->next_approvers ?? []);
+        })->values();
 
         if ($request->wantsJson()) {
             return response()->json([
