ca63b7995682ce404c2d40e1c8acb79611315fe4
jafdigital2020 <marketing@jafdigital.co>
2025-09-29 16:59:16 +0800

    add invoices generate to scheduler

--- routes/scheduler.php
+++ routes/scheduler.php
@@ -5,4 +5,5 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('salary:activate')->daily();
 Schedule::command('geofence:expire')->daily();
 Schedule::command('attendance:mark-absent')->hourly();
+Schedule::command('invoices:generate')->dailyAt('09:00');
 Schedule::command('leave:convert-to-cash')->yearlyOn(12, 31, '00:00');
