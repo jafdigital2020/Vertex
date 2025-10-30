dcc883f39606737b427343b51778b2e5dcbdfe36
John Patrick Paguia <johnpatrickpaguia@JAF---Patrick.local>
2025-10-30 16:14:26 +0800

    Change Footer Copyright 10/30/2025

--- resources/views/layout/partials/footer-company.blade.php
+++ resources/views/layout/partials/footer-company.blade.php
@@ -1,5 +1,5 @@
         <div class="footer d-sm-flex align-items-center justify-content-between border-top bg-white p-3">
-            <p class="mb-0">2025 &copy; Timora.</p>
+            <p class="mb-0">2025 &copy; Timora By JAF Digital Group Inc.</p>
             <p>Designed &amp; Developed By <a href="https://jafdigital.co/" class="text-primary" target="_blank">JAF
                     Digital Group Inc.</a></p>
         </div>
