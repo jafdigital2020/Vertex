f888851a36443b82b357549915a8663539a2fe4f
DenciaGabaon <gabaondenden@gmail.com>
2025-12-09 14:25:34 +0800

    Modify Branch AuthUser

--- app/Http/Controllers/Tenant/Branch/BranchController.php
+++ app/Http/Controllers/Tenant/Branch/BranchController.php
@@ -23,7 +23,7 @@ class BranchController extends Controller
         if (Auth::guard('global')->check()) {
             return Auth::guard('global')->user();
         }
-        return Auth::guard('web')->user();
+        return Auth::user();
     }
 
     public function branchIndex(Request $request)
