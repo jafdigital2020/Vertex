3125798a377e2a40002fa38fbeee1a7f28cba170
DenciaGabaon <gabaondenden@gmail.com>
2025-12-17 16:29:18 +0800

    Modify Department AuthUser [12-17-25 : Denden]

--- app/Http/Controllers/Tenant/DepartmentController.php
+++ app/Http/Controllers/Tenant/DepartmentController.php
@@ -25,7 +25,7 @@ class DepartmentController extends Controller
         if (Auth::guard('global')->check()) {
             return Auth::guard('global')->user();
         }
-        return Auth::guard('web')->user();
+        return Auth::user();
     }
 
     public function departmentIndex(Request $request)
